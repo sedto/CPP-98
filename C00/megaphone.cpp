e33238c5d20a71276f40e982f1ad2061ab7724ab
Votre sedto <sedto@gmail.com>
2025-10-20 21:58:57 +0200

    C00, EX00 megaphone is done

---
+++ C00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dibsejra <dibsejra@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 20:09:01 by dibsejra          #+#    #+#             */
+/*   Updated: 2025/10/20 21:20:36 by dibsejra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return(1);
+	}
+	for (int i = 1; i < argc ; i++)
+	{
+		std::string str = argv[i];
+		for(size_t j = 0; j < str.length(); j++)
+		{
+			str[j] = std::toupper(str[j]);
+		}
+		std::cout << str;
+		if(i < argc -1)
+			std::cout << ' ';
+	}
+	std::cout << '\n';
+	return 0;
+}
