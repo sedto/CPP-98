ce0d6c328c9a621f57a7dff94de9db97a5758bd9
Votre sedto <sedto@gmail.com>
2025-10-28 20:58:37 +0100

    C01 ex02 done memory management + introduction of reference

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dibsejra <dibsejra@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 20:48:32 by dibsejra          #+#    #+#             */
+/*   Updated: 2025/10/28 20:57:36 by dibsejra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <string>
+#include <cctype>
+
+int main()
+{
+	std::string sent = "HI THIS IS BRAIN";
+	std::string *ptr = &sent;
+	std::string &ref = sent;
+	
+	std::cout << &sent << std::endl;
+	std::cout << ptr << std::endl;
+	std::cout << &ref << std::endl;
+
+	std::cout << sent << std::endl;
+	std::cout << *ptr << std::endl;
+	std::cout << ref << std::endl;
+}
