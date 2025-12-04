f133669a2a7b98dd860e710da59e6dca5075ea96
sedto <dibran.sejrani@gmail.com>
2025-12-04 10:09:05 +0100

    adding some small fix on CPP03

--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dibsejra <dibsejra@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 16:00:00 by dibsejra          #+#    #+#             */
-/*   Updated: 2025/10/29 16:00:00 by dibsejra         ###   ########.fr       */
+/*   Updated: 2025/12/04 09:21:48 by dibsejra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,38 @@
 
 int main(void)
 {
-	std::cout << "=== Creating ClapTrap ===" << std::endl;
+	std::cout << "Creating ClapTrap" << std::endl;
 	ClapTrap clap("Bob");
 	std::cout << std::endl;
 
-	std::cout << "=== Creating ScavTrap ===" << std::endl;
+	std::cout << "Creating ScavTrap" << std::endl;
 	ScavTrap scav("Alice");
 	std::cout << std::endl;
 
-	std::cout << "=== Creating FragTrap ===" << std::endl;
+	std::cout << "Creating FragTrap" << std::endl;
 	FragTrap frag("Charlie");
 	std::cout << std::endl;
 
-	std::cout << "=== Testing ClapTrap ===" << std::endl;
+	std::cout << "Testing ClapTrap" << std::endl;
 	clap.attack("target");
 	clap.takeDamage(5);
 	clap.beRepaired(3);
 	std::cout << std::endl;
 
-	std::cout << "=== Testing ScavTrap ===" << std::endl;
+	std::cout << "Testing ScavTrap" << std::endl;
 	scav.attack("enemy");
 	scav.takeDamage(30);
 	scav.beRepaired(20);
 	scav.guardGate();
 	std::cout << std::endl;
 
-	std::cout << "=== Testing FragTrap ===" << std::endl;
+	std::cout << "Testing FragTrap" << std::endl;
 	frag.attack("monster");
 	frag.takeDamage(50);
 	frag.beRepaired(25);
 	frag.highFivesGuys();
 	std::cout << std::endl;
 
-	std::cout << "=== Destruction (reverse order) ===" << std::endl;
+	std::cout << "Destruction (reverse order)" << std::endl;
 	return 0;
 }
